d28b0cf58333c4496060b00ae932552291a7f450
John R. D'Orazio <priest@johnromanodorazio.com>
2024-10-05 12:23:56 +0200

    add doc blocks

--- src/Enum/LitColor.php
+++ src/Enum/LitColor.php
@@ -4,6 +4,24 @@ namespace LiturgicalCalendar\AlexaNewsBrief\Enum;
 
 use LiturgicalCalendar\AlexaNewsBrief\Enum\LitLocale;
 
+/**
+ * The LitColor class is an enumeration of valid liturgical colors.
+ *
+ * Liturgical colors are colors that are used to decorate the church and
+ * vestments on specific days and feasts in the liturgical calendar.
+ *
+ * The values of the enumeration are:
+ *  - GREEN: Green is used on most ordinary Sundays.
+ *  - PURPLE: Purple is used during the Advent and Lent seasons.
+ *  - WHITE: White is used on most holy days and feasts, and during the
+ *  Christmas and Easter seasons.
+ *  - RED: Red is used on Pentecost and on feasts of the martyrs.
+ *  - PINK: Pink is used only on Laetare Sunday (the 4th Sunday of Lent)
+ *  and on Gaudete Sunday (the 3rd Sunday of Advent).
+ *
+ * The class contains methods for checking if a given value or array of
+ * values is valid.
+ */
 class LitColor
 {
     public const GREEN     = "green";
@@ -13,6 +31,14 @@ class LitColor
     public const PINK      = "pink";
     public static array $values = [ "green", "purple", "white", "red", "pink" ];
 
+    /**
+     * Returns true if the given value is a valid liturgical color.
+     *
+     * If $value is a comma-separated string of values, it will be split into an
+     * array and passed to {@see LitColor::areValid()}.
+     * @param string $value
+     * @return bool
+     */
     public static function isValid(string $value)
     {
         if (strpos($value, ',')) {
@@ -21,11 +47,23 @@ class LitColor
         return in_array($value, self::$values);
     }
 
+    /**
+     * Returns true if all of the given values are valid liturgical colors.
+     * @param array $values
+     * @return bool
+     */
     public static function areValid(array $values)
     {
         return empty(array_diff($values, self::$values));
     }
 
+    /**
+     * Translate a liturgical color to the given locale.
+     *
+     * @param string $value
+     * @param string $locale
+     * @return string
+     */
     public static function i18n(string $value, string $locale): string
     {
         switch ($value) {
